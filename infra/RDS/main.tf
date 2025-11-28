6dafe43b7c177642dcb44be8453c697c33c2108a
Tianfan Yang <156636757+ty2492tianfan@users.noreply.github.com>
2025-11-27 22:17:45 -0800

    Enable deletion protection and final snapshot

--- infra/RDS/main.tf
+++ infra/RDS/main.tf
@@ -80,8 +80,8 @@ resource "aws_db_instance" "main" {
   backup_window          = "03:00-04:00"
   maintenance_window     = "mon:04:00-mon:05:00"
 
-  skip_final_snapshot = true # Set to false for production
-  deletion_protection = false
+  skip_final_snapshot = false # Set to false for production
+  deletion_protection = true
 
   enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
 
