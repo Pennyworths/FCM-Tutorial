2be993999361320c095a007708298c8c7974e99c
杨天凡 <ty2492@nyu.edu>
2025-11-30 16:02:27 -0800

    Firebase+APIbase_setup

---
+++ android/app/src/main/java/com/example/fcmplayground/ApiRoutes.kt
@@ -0,0 +1,9 @@
+package com.example.fcmplayground
+
+object ApiRoutes {
+    const val DEVICES_REGISTER = "/devices/register"
+    const val MESSAGES_SEND = "/messages/send"
+    const val TEST_ACK = "/test/ack"
+    const val TEST_STATUS = "/test/status"
+}
+
